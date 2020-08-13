3cda42abcad6581b53f21b5f2423ebd5b6a4b367
Benjamin Kwame Amponsah <bamponsah01@af.etcred.com>
2020-08-13 18:07:08 +0100

    added create inbound

--- src/com/selenium_for_sap/Main.java
+++ src/com/selenium_for_sap/Main.java
@@ -8,8 +8,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Main {
 
   public WebDriver obj;
-  private static String USERNAME = "Georgea01";
-  private static String PASSWORD = "Ecom12345";
+  private static String USERNAME = "MichaelK01";
+  private static String PASSWORD = "Welcome01";
 
 
   public void invokeBrowser() {
@@ -23,8 +23,10 @@ public class Main {
       obj.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
 
       obj.get(
-          "https://my344821.sapbydesign.com/sap/public/ap/ui/repository/SAP_UI/HTMLOBERON5/client.html?client_type=html&app.component=/SAP_UI_CT/Main/root.uiccwoc&rootWindow=X&redirectUrl=/sap/public/ap/ui/runtimehttps://my344821.sapbydesign.com/sap/public/ap/ui/repository/SAP_UI/HTMLOBERON5/client.html?client_type=html&app.component=/SAP_UI_CT/Main/root.uiccwoc&rootWindow=X&redirectUrl=/sap/public/ap/ui/runtime");
+          "https://my348520.sapbydesign.com/sap/public/ap/ui/repository/SAP_UI/HTMLOBERON5/client.html?client_type=html&app.component=/SAP_UI_CT/Main/root.uiccwoc&rootWindow=X&redirectUrl=/sap/public/ap/ui/runtime");
       mLogin();
+      Thread.sleep(40000);
+      createInbound();
     } catch (Exception e) {
       e.printStackTrace();
     }
@@ -35,6 +37,22 @@ public class Main {
       obj.findElement(By.id("__control0-user-inner")).sendKeys(USERNAME);
       obj.findElement(By.id("__control0-pass-inner")).sendKeys(PASSWORD);
       obj.findElement(By.id("__control0-logonBtn")).click();
+      Thread.sleep(5000);
+      if(obj.findElement(By.id("__control1-continueBtn")) != null){
+//        check the clear all session
+//        obj.findElement(By.id("__box0-CB")).click();
+        obj.findElement(By.id("__control1-continueBtn")).click();
+      }
+    }catch (Exception e){
+      e.printStackTrace();
+    }
+  }
+
+  public void createInbound(){
+    try {
+      obj.findElement(By.id("__item25")).click();
+      Thread.sleep(3000);
+      obj.findElement(By.xpath("//div[start-with(@id, ‘__item’)]")).click();
     }catch (Exception e){
       e.printStackTrace();
     }
