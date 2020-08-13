e5807db1411089b5ef01a16bc9ab1a69c9ceb38f
Benjamin Kwame Amponsah <bamponsah01@af.etcred.com>
2020-08-13 09:47:57 +0100

    added login function

--- src/com/selenium_for_sap/Main.java
+++ src/com/selenium_for_sap/Main.java
@@ -7,30 +7,41 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
 
-    public WebDriver obj;
+  public WebDriver obj;
+  private static String USERNAME = "Georgea01";
+  private static String PASSWORD = "Ecom12345";
 
-    public  void invokeBrowser(){
-        try {
-            System.out.println("Hello");
-            System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
-            obj = new ChromeDriver();
-            obj.manage().deleteAllCookies();
-            obj.manage().window().maximize();
-            obj.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-            obj.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
 
-            obj.get("http://www.google.com");
-        } catch (Exception e){
-            e.printStackTrace();
-        }
+  public void invokeBrowser() {
+    try {
+      System.out.println("Hello");
+      System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
+      obj = new ChromeDriver();
+      obj.manage().deleteAllCookies();
+      obj.manage().window().maximize();
+      obj.manage().timeouts().implicitlyWait(120, TimeUnit.SECONDS);
+      obj.manage().timeouts().pageLoadTimeout(120, TimeUnit.SECONDS);
+
+      obj.get(
+          "https://my344821.sapbydesign.com/sap/public/ap/ui/repository/SAP_UI/HTMLOBERON5/client.html?client_type=html&app.component=/SAP_UI_CT/Main/root.uiccwoc&rootWindow=X&redirectUrl=/sap/public/ap/ui/runtimehttps://my344821.sapbydesign.com/sap/public/ap/ui/repository/SAP_UI/HTMLOBERON5/client.html?client_type=html&app.component=/SAP_UI_CT/Main/root.uiccwoc&rootWindow=X&redirectUrl=/sap/public/ap/ui/runtime");
+      mLogin();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
-    public  void mSearch(){
-        
+  public void mLogin() {
+    try {
+      obj.findElement(By.id("__control0-user-inner")).sendKeys(USERNAME);
+      obj.findElement(By.id("__control0-pass-inner")).sendKeys(PASSWORD);
+      obj.findElement(By.id("__control0-logonBtn")).click();
+    }catch (Exception e){
+      e.printStackTrace();
     }
+  }
 
-    public static void main(String[] args) {
-        Main mTest = new Main();
-        mTest.invokeBrowser();
-    }
+  public static void main(String[] args) {
+    Main mTest = new Main();
+    mTest.invokeBrowser();
+  }
 }
