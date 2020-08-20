306015678ec61b2f87dcb4fd35876b1b36a0bf46
benjelino <scientyfic.ba@gmail.com>
2020-08-20 23:38:00 +0100

    configured the functions to create both inbound and outbound at a loop of the json data

--- src/main/java/com/selenium_for_sap/Main.java
+++ src/main/java/com/selenium_for_sap/Main.java
@@ -15,7 +15,6 @@ public class Main {
   public WebDriver obj;
   private static String USERNAME = "MichaelK01";
   private static String PASSWORD = "Welcome1";
-  private int rejections = 50;
   private boolean rejected = false;
 
 
@@ -32,13 +31,11 @@ public class Main {
       obj.get(
           "https://my347340.sapbydesign.com/sap/public/ap/ui/repository/SAP_UI/HTMLOBERON5/client.html?client_type=html&app.component=/SAP_UI_CT/Main/root.uiccwoc&rootWindow=X&redirectUrl=/sap/public/ap/ui/runtime");
       mLogin();
-      Thread.sleep(40000);
+      Thread.sleep(35000);
       for(Data p: list) {
-//        checkInbound(p);
-        checkOutbound(p);
+        checkInbound(p);
         Thread.sleep(30000);
-//        fillInbound(p);
-        fillOutbound(p);
+        fillInbound(p);
         Json writer = new Json();
         writer.addData(p);
       }
@@ -67,16 +64,18 @@ public class Main {
   public void checkInbound(Data data){
     try {
       obj.findElement(By.id("__item34")).click();
-      Thread.sleep(2000);
+      Thread.sleep(1000);
       obj.findElement(By.xpath("/descendant::div[starts-with(@id, '__item')][2]")).click();
-      Thread.sleep(20000);
+      Thread.sleep(17000);
       obj.findElement(By.xpath("//input[starts-with(@id, '__pane') and contains(@id, '-searchField-I')]")).sendKeys("*" + data.getSecondaryid() + "*");
       obj.findElement(By.xpath("//div[starts-with(@id, '__pane') and contains(@id, '-searchField-search')]")).click();
-      Thread.sleep(10000);
+      Thread.sleep(9000);
       String rowCount = obj.findElement(By.xpath("//table[@class='sapBUiListTab']")).getAttribute("aria-rowcount");
-      if(Integer.parseInt(rowCount) == 1){
+      if (Integer.parseInt(rowCount) == 1) {
         System.out.println("row count is equal to 1");
         obj.findElement(By.xpath("//span[.='Post Goods Receipt']")).click();
+      } else {
+        System.out.println("District not found");
       }
 
     }catch (Exception e){
@@ -93,11 +92,11 @@ public class Main {
       obj.findElement(By.xpath("/descendant::input[starts-with(@id, '__field') and contains(@id, '-inner')][4]")).sendKeys(data.getArrivaldate());
       obj.findElement(By.xpath("/descendant::input[starts-with(@id, '__field') and contains(@id, '-inner')][5]")).sendKeys(data.getOffloadingdate());
       ArrayList<String[]> smnq = new ArrayList<String[]>();
-      Thread.sleep(15000);
+      Thread.sleep(12000);
       obj.findElement(By.xpath("//span[.='Propose Quantities']")).click();
       JavascriptExecutor jse = (JavascriptExecutor) obj;
       jse.executeScript("document.querySelector('table th:last-child').scrollIntoView();");
-      Thread.sleep(10000);
+      Thread.sleep(3000);
       List<WebElement> rows = obj.findElements(By.xpath("//table[@class='sapBUiListTab']/tbody/tr"));
       int count = rows.size();
       System.out.println(count);
@@ -112,26 +111,26 @@ public class Main {
         String quantity= obj.findElement(By.xpath(row)).getAttribute("value");
         obj.findElement(By.xpath(grade)).clear();
         obj.findElement(By.xpath(grade)).sendKeys(data.getGrade());
-        Thread.sleep(15000);
+        Thread.sleep(12000);
         obj.findElement(By.xpath(label)).click();
         System.out.println("label button clicked");
-        Thread.sleep(20000);
-        if(rejections!= 0 && !rejected){
-          if(Integer.parseInt(quantity)>rejections){
+        Thread.sleep(8000);
+        if(data.getRejections()!= 0 && !rejected){
+          if(Integer.parseInt(quantity)>data.getRejections()){
             String addedRow = "//td[starts-with(@id, '__table') and contains(@id,"+"'" + "_r" + i + "_c6"
                 + "'" +")]/div/div/div/div/input";
             String addedGrade = "//td[starts-with(@id, '__table') and contains(@id,"+"'" + "_r" + i + "_c7"
                 + "'" +")]/div/div/div/input";
-            int qauntityLeft = Integer.parseInt(quantity) - rejections;
+            int qauntityLeft = Integer.parseInt(quantity) - data.getRejections();
             obj.findElement(By.xpath(row)).clear();
             obj.findElement(By.xpath(row)).sendKeys(String.valueOf(qauntityLeft));
-            Thread.sleep(20000);
+            Thread.sleep(2000);
             obj.findElement(By.xpath("//span[.='Add Sub Item']")).click();
-            Thread.sleep(15000);
-            jse.executeScript("document.querySelector('table th:last-child').scrollIntoView();");
-            Thread.sleep(5000);
-            obj.findElement(By.xpath(addedRow)).sendKeys(String.valueOf(rejections));
-            Thread.sleep(5000);
+            Thread.sleep(14000);
+//            jse.executeScript("document.querySelector('table th:last-child').scrollIntoView();");
+//            Thread.sleep(5000);
+            obj.findElement(By.xpath(addedRow)).sendKeys(String.valueOf(data.getRejections()));
+            Thread.sleep(3000);
             obj.findElement(By.xpath(addedGrade)).clear();
             obj.findElement(By.xpath(addedGrade)).sendKeys("DS_TD_AGL");
             Thread.sleep(5000);
@@ -140,7 +139,7 @@ public class Main {
             i++;
             count = count +2;
             rejected=true;
-          }else  if(Integer.parseInt(quantity)==rejections){
+          }else  if(Integer.parseInt(quantity)==data.getRejections()){
             obj.findElement(By.xpath(grade)).clear();
             obj.findElement(By.xpath(grade)).sendKeys("DS_TD_AGL");
             Thread.sleep(5000);
@@ -153,11 +152,12 @@ public class Main {
             Thread.sleep(5000);
             obj.findElement(By.xpath(label)).click();
             currentgrade = obj.findElement(By.xpath(grade)).getAttribute("value");
-            rejections = rejections - Integer.parseInt(quantity);
+            int rejection = data.getRejections() - Integer.parseInt(quantity);
+            data.setRejections(rejection);
           }
         }
         if (!currentgrade.equals("DS_TD_AGL")) {
-          Thread.sleep(8000);
+          Thread.sleep(5000);
           String stationMark = obj.findElement(By.xpath(
               "/descendant::div[@class='sapBUiCctsMinWidth sapBUiCctsFormIdentifierIDAndDescription'][2]/div/div/input"))
               .getAttribute("value");
@@ -170,10 +170,10 @@ public class Main {
         }
       }
       obj.findElement(By.xpath("//span[.='Save and Close']")).click();
-      Thread.sleep(170000);
+      Thread.sleep(100000);
       String inboundID = obj.findElement(By.xpath("/descendant::div[@class='sapMMsgStripMessage'][1]/span[1]")).getText();
       data.setInboundid(inboundID);
-
+      checkOutbound(data);
       System.out.println(inboundID);
     }catch(Exception e){
       e.printStackTrace();
@@ -187,35 +187,41 @@ public class Main {
       String[] splits = station.split(" ");
       System.out.println();
       obj.findElement(By.id("__item52")).click();
-      Thread.sleep(2000);
+      Thread.sleep(1000);
       obj.findElement(By.xpath("/descendant::div[starts-with(@id, '__item')][3]")).click();
-      Thread.sleep(20000);
+      Thread.sleep(17000);
       obj.findElement(By.xpath("//span[starts-with(@id, '__pane') and contains(@id, '-defaultSetDDLB-arrow')]")).click();
       Thread.sleep(1000);
       obj.findElement(By.xpath("/descendant::ul[starts-with(@id, '__list')]/li[11]")).click();
       Thread.sleep(3000);
       obj.findElement(By.xpath("//a[.='Click here to execute the query']")).click();
-      Thread.sleep(15000);
+      Thread.sleep(12000);
       obj.findElement(By.xpath("//input[starts-with(@id, '__pane') and contains(@id, '-searchField-I')]")).sendKeys(splits[0]);
       obj.findElement(By.xpath("//div[starts-with(@id, '__pane') and contains(@id, '-searchField-search')]")).click();
-      Thread.sleep(15000);
+      Thread.sleep(12000);
       String rowCount = obj.findElement(By.xpath("//table[@class='sapBUiListTab']")).getAttribute("aria-rowcount");
-      if(Integer.parseInt(rowCount) == 1){
-        System.out.println("row count is equal to 1");
+      if (Integer.parseInt(rowCount) >= 1) {
         obj.findElement(By.xpath("//span[.='Post Goods Issue']")).click();
+      } else {
+        System.out.println("Something Unexpected happened");
       }
+      fillOutbound(data);
     }catch (Exception e){
       e.printStackTrace();
     }
   }
 
   public void fillOutbound(Data data) throws InterruptedException {
-
+    String label = "//label[starts-with(@for, '__item') and contains(@for, '-0-dli-value')]";
     //Freight input
     obj.findElement(By.xpath("/descendant::input[starts-with(@id, '__input') and contains(@id, '-inner')][1]")).sendKeys("KGL - Kiteko Ghana Ltd.");
+    Thread.sleep(3000);
+    obj.findElement(By.xpath(label)).click();
+    Thread.sleep(5000);
     //Shipment Date
     obj.findElement(By.xpath("/descendant::input[starts-with(@id, '__field') and contains(@id, '-inner')][1]")).clear();
     obj.findElement(By.xpath("/descendant::input[starts-with(@id, '__field') and contains(@id, '-inner')][1]")).sendKeys(data.getDate());
+    Thread.sleep(5000);
     //CTOR input
     obj.findElement(By.xpath("/descendant::input[starts-with(@id, '__field') and contains(@id, '-inner')][3]")).sendKeys(String.valueOf(data.getCtor()));
     //Actual weight
@@ -236,13 +242,8 @@ public class Main {
 
     for(int i=1; i<list; i++) {
       obj.findElement(By.xpath("//span[.='Add Sub Item']")).click();
-      try {
-        Thread.sleep(15000);
-      } catch (InterruptedException e) {
-        e.printStackTrace();
-      }
+      Thread.sleep(12000);
     }
-    Thread.sleep(10000);
     for (String[] item: stationnquantity){
       String station = (String) item[1];
       String[] splits = station.split(" ");
@@ -258,22 +259,27 @@ public class Main {
               "//td[starts-with(@id, '__table') and contains(@id," + "'" + "_r" + i + "_c9"
                   + "'" + ")]/div/div/div/input";
 
+
+          obj.findElement(By.xpath(stationmark)).sendKeys(splits[0]);
+          Thread.sleep(2000);
           obj.findElement(By.xpath(addedRow)).clear();
           obj.findElement(By.xpath(addedRow)).sendKeys(item[0]);
-          Thread.sleep(5000);
-          obj.findElement(By.xpath(stationmark)).sendKeys(item[1]);
+          obj.findElement(By.xpath("//span[.='Details: Item 10- 1-Cocoa']")).click();
+          Thread.sleep(2000);
         }
       }else {
         obj.findElement(By.xpath("//td[starts-with(@id, '__table') and contains(@id,'_r0_c9')]/div/div/div/input")).sendKeys(splits[0]);
-        obj.findElement(By.xpath("//span[.='Remove Sub Item']")).click();
         Thread.sleep(5000);
         obj.findElement(By.xpath("//td[starts-with(@id, '__table') and contains(@id,'_r0_c7')]/div/div/div/div/input")).sendKeys(item[0]);
+        obj.findElement(By.xpath("//span[.='Details: Item 10- 1-Cocoa']")).click();
       }
 
     }
-    Thread.sleep(10000);
+    Thread.sleep(6000);
     obj.findElement(By.xpath("//span[.='Release']")).click();
-
+    Thread.sleep(40000);
+    String outboundID = obj.findElement(By.xpath("/descendant::div[@class='sapMMsgStripMessage'][1]/span[1]")).getText();
+    data.setOutboundid(outboundID);
   }
 
   public static void main(String[] args) {
